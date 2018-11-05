c989b25b4c288e047dc6d2dd4e3c86bc047ad5cb
112Nick <mrnickrus@gmail.com>
2018-11-05 03:40:03 +0300

    profile screen

---
+++ app/src/main/java/ru/fintech/tinkoff/fintechportal/AuthorizationActivity.kt
@@ -0,0 +1,6 @@
+package ru.fintech.tinkoff.fintechportal
+
+import android.support.v7.app.AppCompatActivity
+
+class AuthorizationActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/ru/fintech/tinkoff/fintechportal/ProfileFragment.kt
@@ -0,0 +1,13 @@
+package ru.fintech.tinkoff.fintechportal
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
